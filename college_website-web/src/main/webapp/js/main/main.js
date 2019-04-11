470c416e48880188fab2e284c8f296e3b39673e7
Administrator <https://github.com/wangkunswpu2018/college_website.git>
2019-04-11 20:16:13 +0800

    BannerImgInfo除Service层的文件

--- college_website-web/src/main/webapp/js/main/main.js
+++ college_website-web/src/main/webapp/js/main/main.js
@@ -3,17 +3,17 @@ var main={};
 
 main.homepageGoTo=function(url1,url2,url3,param){
 	$.post(url1, param, function(data) {
-		$('#main-page-content').html(data);
+		$('#main-page-content1').html(data);
 
 		document.body.scrollTop=0;
 	});
 	$.post(url2, param, function(data) {
-		$('#main-page-content').append(data);
+		$('#main-page-content2').html(data);
 
 		document.body.scrollTop=0;
 	});
 	$.post(url3, param, function(data) {
-		$('#main-page-content').append(data);
+		$('#main-page-content3').html(data);
 
 		document.body.scrollTop=0;
 	});
@@ -21,7 +21,9 @@ main.homepageGoTo=function(url1,url2,url3,param){
 
 main.menuGoTo=function(url,param){
 	$.post(url, param, function(data) {
-	  $('#main-page-content').html(data);	
+	  $('#main-page-content1').html(data);
+	  $('#main-page-content2').empty();
+	  $('#main-page-content3').empty();
 
 	  document.body.scrollTop=0;
 	});
