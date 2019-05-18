dd3256e28a60dc365c18db62359f9a50d051e949
LXH <2419619918@qq.com>
2019-05-18 10:22:39 +0800

    将新闻聚焦板块链接到数据库，添加学生中心的登录跳转页面

--- college_website-web/src/main/java/cn/edu/swpu/info/college_website/web/IndexController.java
+++ college_website-web/src/main/java/cn/edu/swpu/info/college_website/web/IndexController.java
@@ -1,7 +1,9 @@
 package cn.edu.swpu.info.college_website.web;
 
 import cn.edu.swpu.info.college_website.common.PinContext;
+import cn.edu.swpu.info.college_website.domain.Message;
 import cn.edu.swpu.info.college_website.domain.OpsFunction;
+import cn.edu.swpu.info.college_website.service.MessageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -21,14 +24,21 @@ public class IndexController {
 
 	@RequestMapping(value = "", method = { RequestMethod.GET })
 	public String index(HttpServletRequest request, HttpServletResponse response, Model view) {
+		List<Message> messagesData = messageService.selectMessagelist(new Message());
+		view.addAttribute("messages", messagesData);
 		List<OpsFunction> functionData = PinContext.getFunctionData();
 //		//加载用户资源
 //		view.addAttribute("erp", pin);
 		view.addAttribute("functions", functionData);
 		return "main/main";
 	}
+
+	@Resource
+	private MessageService messageService;
 	@RequestMapping(value = "/index", method = { RequestMethod.GET })
 	public String index1(HttpServletRequest request, HttpServletResponse response, Model view) {
+		List<Message> messagesData = messageService.selectMessagelist(new Message());
+		view.addAttribute("messages", messagesData);
 		return "common/index";
 	}
 
