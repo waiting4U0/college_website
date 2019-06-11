ca9f9a57445063ccdf7b5a0d30cd144c09bd420a
Administrator <https://github.com/wangkunswpu2018/college_website.git>
2019-06-11 19:37:32 +0800

    Merge branch 'master' of X:\Users\Administrator\IdeaProjects\college_website with conflicts.

---
+++ college_website-dao/src/main/java/cn/edu/swpu/info/college_website/dao/OpsAdminDao.java
@@ -0,0 +1,14 @@
+
+package cn.edu.swpu.info.college_website.dao;
+
+
+import cn.edu.swpu.info.college_website.dao.base.BaseDao;
+import cn.edu.swpu.info.college_website.domain.OpsAdmin;
+
+/**
+ * OpsAdminDao 接口
+ */
+public interface OpsAdminDao extends BaseDao<OpsAdmin,Long> {
+	//自定义扩展
+	
+}
